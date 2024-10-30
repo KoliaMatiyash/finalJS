5da27c9e17596e2e7b1e776602805666e1a2f358
KoliaMatiyash <162808335+KoliaMatiyash@users.noreply.github.com>
2024-10-30 02:33:51 +0200

    take_id

---
+++ app.js
@@ -0,0 +1,11 @@
+// calendar main info
+const calendar = document.getElementById("calendarDays");
+const titleCalendar = document.getElementById("titleTxt");
+// event window
+const eventList = document.getElementById("eventList");
+const eventForm = document.getElementById("eventForm");
+// bnts
+const prevBtn = document.getElementById("btnPrev");
+const nextBtn = document.getElementById("btnNext");
+const closeEwindow = document.getElementById("closeModel");
+const eventBtn = document.getElementById("saveEvent");
